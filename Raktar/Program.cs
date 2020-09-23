048d1157ceac64aa5d70b400998cb1234e6b6cf5
ViczianGabor <viczian.gabor13@gmail.com>
2020-09-23 14:56:13 +0200

    beolvasas kezdet

--- Raktar/Program.cs
+++ Raktar/Program.cs
@@ -3,14 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Raktar
 {
     class Program
     {
+
+        static void beolvas_raktar()
+        {
+            StreamReader sr = new StreamReader("raktar.csv");
+            while (!sr.EndOfStream) 
+            {
+                /*
+                 sor[0] -> kod
+                 sor[1] -> ár stb
+                 
+                 */
+                string[] sor = sr.ReadLine().Split(';');
+                Console.WriteLine(sor[0]);
+                
+
+
+            }
+
+            sr.Close();
+        }
         static void Main(string[] args)
         {
-
+            beolvas_raktar();
             Termek t = new Termek("P01","XL",1500,10);
 
 
